dd6b88ebee33f0d0dffe1731ac86a7b0c0ef2d8e
Christoph Heindl <christoph.heindl@gmail.com>
2016-07-03 07:54:02 +0200

    experimenting

--- inc/jtypes/jtypes.h
+++ inc/jtypes/jtypes.h
@@ -19,55 +19,80 @@
 namespace jtypes {
 
     using mapbox::util::variant;
-    
+
     class object;
     class array;
     class value;
+    class undefined {};
     
+    typedef std::nullptr_t null;
+
     class object {
     public:
         std::unordered_map<std::string, value> m;
     };
-    
+
     class array {
     public:
         std::vector<value> a;
-        
+
         array() = default;
-        
+
         template<class T>
-        array(const std::initializer_list<T> &t) {
-            for (auto && v : t) {
-                a.push_back(value(v));
-            }
-        }
+        array(const std::initializer_list<T> &t);
         
-        array(const std::initializer_list<value> &t) {
-            a = t;
-        }
+        array(const std::initializer_list<value> &t);
         
+        const value &operator[](size_t idx) const;
+        value &operator[](size_t idx);
     };
-    
-    
+
+
     class value {
     public:
-        typedef variant<int, std::string, object, array> var;
+        typedef variant<null, undefined, int, std::string, object, array> var;
         var v;
+
+        value()
+        :v(undefined())
+        {}
         
-        value() = default;
-        
+        const static value theUndefined;
+
         value(var && v_)
         :v(std::move(v_)) {}
     };
+
+    const value value::theUndefined = value(undefined());
+
+    template<class T>
+    array::array(const std::initializer_list<T> &t) {
+        for (auto && v : t) {
+            a.push_back(value(v));
+        }
+    }
     
+    array::array(const std::initializer_list<value> &t) {
+        a = t;
+    }
+
+    const value &array::operator[](size_t idx) const {
+        if (idx < a.size()){
+            return value::theUndefined;
+        } else {
+            return a[idx];
+        }
+    }
+    
+    value &array::operator[](size_t idx) {
+        if (idx < a.size()){
+            a.resize(idx + 1);
+        }
+        return a[idx];
+    }
 
     
-    
-    
 
-    
-
-    
 }
 
 #endif
