76b62dedf59a075ec9b850a21ebc5accd1b8da82
Christoph Heindl <christoph.heindl@gmail.com>
2016-07-07 20:35:52 +0200

    Refactoring and start of using var interface.

--- inc/jtypes/jtypes.h
+++ inc/jtypes/jtypes.h
@@ -15,82 +15,302 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <cstdint>
+#include <type_traits>
+#include <utility>
+#include <stdexcept>
+
+#include <iostream>
+
 
 namespace jtypes {
 
     using mapbox::util::variant;
-
-    class object;
-    class array;
-    class value;
-    class undefined {};
     
-    typedef std::nullptr_t null;
-
-    class object {
-    public:
-        std::unordered_map<std::string, value> m;
+    class var;
+    
+    typedef std::nullptr_t null_t;
+    typedef bool bool_t;
+    typedef std::string string_t;
+    typedef std::vector<var> array_t;
+    typedef std::unordered_map<std::string, var> object_t;
+    typedef int64_t sint_t;
+    typedef uint64_t uint_t;
+    typedef double real_t;
+    typedef variant<int64_t, uint64_t, double> number_t;
+    
+    enum class type {
+        null = 0,
+        boolean,
+        number,
+        string,
+        array,
+        object
     };
-
-    class array {
-    public:
-        std::vector<value> a;
-
-        array() = default;
-
-        template<class T>
-        array(const std::initializer_list<T> &t);
-        
-        array(const std::initializer_list<value> &t);
-        
-        const value &operator[](size_t idx) const;
-        value &operator[](size_t idx);
+    
+    enum class number_type {
+        signed_integral,
+        unsigned_integral,
+        real
     };
-
-
-    class value {
+    
+    class bad_access : public std::logic_error
+    {
+        
     public:
-        typedef variant<null, undefined, int, std::string, object, array> var;
-        var v;
+        explicit bad_access(const std::string& what_arg)
+        : logic_error(what_arg) {}
+        
+        explicit bad_access(const char* what_arg)
+        : logic_error(what_arg) {}
+        
+    };
+    
+    namespace detail {
+        template <typename _Tp> struct is_jtypes_type  : public std::false_type {};
+        template <>          struct is_jtypes_type<null_t>  : public std::true_type {};
+        template <>          struct is_jtypes_type<bool_t>  : public std::true_type {};
+        template <>          struct is_jtypes_type<string_t>  : public std::true_type {};
+        template <>          struct is_jtypes_type<array_t>  : public std::true_type {};
+        template <>          struct is_jtypes_type<object_t>  : public std::true_type {};
+        template <>          struct is_jtypes_type<number_t>  : public std::true_type {};
+        
+        
+        template <typename _Tp> struct jtype_to_enum {};
+        template <>          struct jtype_to_enum<null_t> : public std::integral_constant<type, type::null> {};
+        template <>          struct jtype_to_enum<bool_t> : public std::integral_constant<type, type::boolean> {};
+        template <>          struct jtype_to_enum<string_t> : public std::integral_constant<type, type::string> {};
+        template <>          struct jtype_to_enum<array_t> : public std::integral_constant<type, type::array> {};
+        template <>          struct jtype_to_enum<object_t> : public std::integral_constant<type, type::object> {};
+        template <>          struct jtype_to_enum<number_t> : public std::integral_constant<type, type::number> {};
+        
+        template<typename ToType, typename EnableIfType = void>
+        struct coerce_number
+        {};
+        
+        template<>
+        struct coerce_number<bool, void>
+        {
+            bool value = false;
+            
+            template<class T>
+            void operator()(T v) { value = (v != T(0)); }
+        };
+        
+        template<typename ToType>
+        struct coerce_number<ToType, void>
+        {
+            ToType value = ToType(0);
+            
+            template<class T>
+            void operator()(T v) { value = ToType(v); }
+        };
+        
+        
+        template<typename ToType, typename EnableIfType = void>
+        struct coerce
+        {};
+        
+        template<>
+        struct coerce<bool, void>
+        {
+            bool value = false;
+            
+            void operator()(const null_t &v)  { value = false; }
+            void operator()(const bool_t &v)  { value = v; }
+            void operator()(const string_t &v)  { value = !v.empty(); }
+            void operator()(const array_t &v)  { value = true; }
+            void operator()(const object_t &v)  { value = true; }
+            void operator()(const number_t &v)  {
+                coerce_number<bool> cn;
+                apply_visitor(cn, v);
+                value = cn.value;
+            }
+        };
+        
+        template<typename ToType>
+        struct coerce<ToType, typename std::enable_if<std::is_integral<ToType>::value>::type >
+        {
+            ToType value = ToType(0);
+            
+            void operator()(const bool_t &v)  { value = v ? ToType(1) : ToType(0); }
+            void operator()(const string_t &v)  {
+                try {
+                    value = ToType(std::stol(v));
+                } catch (std::exception) {
+                    throw bad_access("Failed to coerce type from string to integral type.");
+                }
+            }
+            
+            void operator()(const number_t &v)  {
+                coerce_number<ToType> cn;
+                apply_visitor(cn, v);
+                value = cn.value;
+            }
+            
+            template<class T>
+            void operator()(const T &t) {
+                throw bad_access("Failed to coerce type to integral type.");
+            }
+        };
+        
+        template<typename ToType>
+        struct coerce<ToType, typename std::enable_if<std::is_floating_point<ToType>::value>::type >
+        {
+            ToType value = ToType(0);
+            
+            void operator()(const bool_t &v)  { value = v ? ToType(1) : ToType(0); }
+            void operator()(const string_t &v)  {
+                try {
+                    value = ToType(std::stod(v));
+                } catch (std::exception) {
+                    throw bad_access("Failed to coerce type from string to floating point type.");
+                }
+            }
+            
+            void operator()(const number_t &v)  {
+                coerce_number<ToType> cn;
+                apply_visitor(cn, v);
+                value = cn.value;
+            }
+            
+            template<class T>
+            void operator()(const T &t) {
+                throw bad_access("Failed to coerce type to integral type.");
+            }
+        };
+    }
+    
 
-        value()
-        :v(undefined())
+    
+    class var
+    {
+    public:
+        
+        // Value initializers
+        
+        var()
+        : _value(nullptr) {}
+        
+        var(std::nullptr_t)
+        : _value(nullptr) {}
+        
+        var(bool v)
+        : _value(v) {}
+        
+        
+        template<typename I>
+        var(I t, typename std::enable_if<std::is_integral<I>::value && std::is_signed<I>::value>::type* unused = 0)
+        : _value(number_t(static_cast<int64_t>(t)))
         {}
         
-        const static value theUndefined;
-
-        value(var && v_)
-        :v(std::move(v_)) {}
+        template<typename I>
+        var(I t, typename std::enable_if<std::is_integral<I>::value && std::is_unsigned<I>::value>::type* unused = 0)
+        : _value(number_t(static_cast<uint64_t>(t)))
+        {}
+        
+        template<typename I>
+        var(I t, typename std::enable_if<std::is_floating_point<I>::value>::type* unused = 0)
+        : _value(number_t(static_cast<double>(t)))
+        {}
+        
+        var(double v)
+        : _value(number_t(v))
+        {}
+        
+        var(const char* v)
+        : _value(std::string(v))
+        {}
+        
+        var(const std::string &v)
+        : _value(v)
+        {}
+        
+        var(std::string &&v)
+        : _value(std::move(v))
+        {}
+        
+        // Array initializers
+        
+        template<typename T>
+        var(std::initializer_list<T> v)
+        {
+            array_t a;
+            for (auto && t : v) {
+                a.emplace_back(var(t));
+            }
+            _value = std::move(a);
+        }
+        
+        template<typename T>
+        var(const std::vector<T> &v)
+        {
+            array_t a;
+            for (auto && t : v) {
+                a.emplace_back(var(t));
+            }
+            _value = std::move(a);
+        }
+        
+        // Dictionary initializers
+        
+        var(std::initializer_list< std::pair<string_t, var> > v)
+        {
+            object_t o;
+            for (auto &&t : v) {
+                o.insert(object_t::value_type(t.first, t.second));
+            }
+            _value = std::move(o);
+        }
+        
+        // Type queries
+        type get_type() const { return (type)_value.which(); }
+        
+        
+        bool_t is_null() const { return _value.which() == (int)type::null; }
+        bool_t is_boolean() const { return _value.which() == (int)type::boolean; }
+        bool_t is_number() const { return _value.which() == (int)type::number; }
+        bool_t is_string() const { return _value.which() == (int)type::string; }
+        bool_t is_array() const { return _value.which() == (int)type::array; }
+        bool_t is_object() const { return _value.which() == (int)type::object; }
+        
+        bool_t is_signed_integral() const { return is_number() && _value.get<number_t>().which() == (int)number_type::signed_integral; }
+        bool_t is_unsigned_integral() const { return is_number() && _value.get<number_t>().which() == (int)number_type::unsigned_integral; }
+        bool_t is_real() const { return is_number() && _value.get<number_t>().which() == (int)number_type::real; }
+        
+        // Getters with coercion
+        
+        explicit operator bool() const {
+            return as<bool>();
+        }
+        
+        template<class T>
+        T as() const {
+            detail::coerce<T> visitor;
+            apply_visitor(visitor, _value);
+            return visitor.value;
+        }
+        
+        
+        /*
+        template<class T>
+        const typename std::enable_if<detail::is_jtypes_type<T>::value, T>::type &
+        get() const {
+            if (_value.which() == (int)detail::jtype_to_enum<T>::value) {
+                return _value.get<T>();
+            } else {
+                throw bad_access("The value held by var does not correspond to requested type in var::get().");
+            }
+            
+        }*/
+        
+        
+        
+    private:
+        typedef variant<null_t, bool_t, number_t, string_t, array_t, object_t> oneof;
+        
+        oneof _value;
     };
-
-    const value value::theUndefined = value(undefined());
-
-    template<class T>
-    array::array(const std::initializer_list<T> &t) {
-        for (auto && v : t) {
-            a.push_back(value(v));
-        }
-    }
-    
-    array::array(const std::initializer_list<value> &t) {
-        a = t;
-    }
-
-    const value &array::operator[](size_t idx) const {
-        if (idx < a.size()){
-            return value::theUndefined;
-        } else {
-            return a[idx];
-        }
-    }
-    
-    value &array::operator[](size_t idx) {
-        if (idx < a.size()){
-            a.resize(idx + 1);
-        }
-        return a[idx];
-    }
-
     
 
 }
--- tests/test_jtypes.cpp
+++ tests/test_jtypes.cpp
@@ -13,14 +13,167 @@ of MIT license. See the LICENSE file for details.
 
 #include <jtypes/jtypes.h>
 
-TEST_CASE("jtypes-should-work")
+TEST_CASE("jtypes can be initialized from simple types")
 {
-    jtypes::value var;
+    {
+        jtypes::var x;
+        REQUIRE(x.is_null());
+    }
     
-    var.v = jtypes::object();
-    var.v.get<jtypes::object>().m["x"] = jtypes::value(3);
-    var.v.get<jtypes::object>().m["a"] = jtypes::value(jtypes::array({1,2,3}));
+    {
+        jtypes::var x(nullptr);
+        REQUIRE(x.is_null());
+    }
     
-    REQUIRE(var.v.get<jtypes::object>().m["a"].v.which() == 3);
+    {
+        jtypes::var x(true);
+        REQUIRE(x.is_boolean());
+    }
     
+    {
+        jtypes::var x(false);
+        REQUIRE(x.is_boolean());
+    }
+    
+    {
+        jtypes::var x(0);
+        REQUIRE(x.is_signed_integral());
+    }
+    
+    {
+        jtypes::var x(3);
+        REQUIRE(x.is_signed_integral());
+    }
+    
+    {
+        jtypes::var x(-3);
+        REQUIRE(x.is_signed_integral());
+    }
+
+    {
+        jtypes::var x(3u);
+        REQUIRE(x.is_unsigned_integral());
+    }
+    
+    {
+        jtypes::var x(3.f);
+        REQUIRE(x.is_real());
+    }
+    
+    {
+        jtypes::var x(3.0);
+        REQUIRE(x.is_real());
+    }
+    
+    {
+        jtypes::var x("hello world");
+        REQUIRE(x.is_string());
+    }
+    
+    {
+        jtypes::var x(std::string("hello world"));
+        REQUIRE(x.is_string());
+    }
 }
+
+TEST_CASE("jtypes can be initialized from arrays")
+{
+    {
+        jtypes::var x({1,2,3});
+        REQUIRE(x.is_array());
+    }
+    
+    {
+        jtypes::var x({jtypes::var(1),jtypes::var(2),jtypes::var(3)});
+        REQUIRE(x.is_array());
+    }
+    
+    {
+        std::vector<int> v = {1, 2, 3};
+        jtypes::var x(v);
+        REQUIRE(x.is_array());
+    }
+    
+    {
+        jtypes::array_t v = {1, 2, 3};
+        jtypes::var x(v);
+        REQUIRE(x.is_array());
+    }
+}
+
+TEST_CASE("jtypes can be initialized from dictionaries")
+{
+    {
+        jtypes::var x = {
+            {"a", 10},
+            {"b", "hello world"}
+        };
+        REQUIRE(x.is_object());
+    }
+    
+    {
+        jtypes::var x = {
+            {"a", jtypes::var(10)},
+            {"b", jtypes::var("hello world")}
+        };
+        REQUIRE(x.is_object());
+    }
+    
+    {
+        jtypes::var x = {
+            {"a", 10},
+            {"b", {{"c", "hello world"}}}
+        };
+        REQUIRE(x.is_object());
+    }
+}
+
+TEST_CASE("jtypes can fetch values without coercion")
+{
+    
+    jtypes::var x = {
+        {"a", 10},
+        {"b", "hello world"}
+    };
+
+}
+
+TEST_CASE("jtypes should be convertible to bool")
+{
+    REQUIRE(!jtypes::var());
+    REQUIRE(!jtypes::var(nullptr));
+    
+    REQUIRE(jtypes::var(true));
+    REQUIRE(!jtypes::var(false));
+    
+    REQUIRE(jtypes::var(1));
+    REQUIRE(!jtypes::var(0));
+    REQUIRE(jtypes::var(1.0));
+    REQUIRE(!jtypes::var(0.0));
+    
+    REQUIRE(jtypes::var("abc"));
+    REQUIRE(!jtypes::var(""));
+    
+    REQUIRE(jtypes::var({1,2,3}));
+    REQUIRE(jtypes::var({{"a", 10}}));
+}
+
+TEST_CASE("jtypes should handle coercion to integral types")
+{
+    
+    REQUIRE_THROWS_AS(jtypes::var().as<int>(), jtypes::bad_access);
+    REQUIRE_THROWS_AS(jtypes::var({1,2,3}).as<int>(), jtypes::bad_access);
+    REQUIRE_THROWS_AS(jtypes::var({{"a",1}}).as<int>(), jtypes::bad_access);
+    
+    REQUIRE(jtypes::var(true).as<int>() == 1);
+    REQUIRE(jtypes::var(false).as<int>() == 0);
+    
+    REQUIRE(jtypes::var(5).as<int>() == 5);
+    REQUIRE(jtypes::var(5u).as<int>() == 5);
+    REQUIRE(jtypes::var(5.5).as<int>() == 5);
+    
+    REQUIRE(jtypes::var(5).as<long>() == 5);
+    REQUIRE(jtypes::var(5.5).as<char>() == (char)5);
+}
+
+
