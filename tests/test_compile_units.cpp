1448a194dd12acd2fab5a221758ebdbd52cd6581
Christoph Heindl <christoph.heindl@gmail.com>
2016-07-17 09:07:20 +0200

    Adding second compile unit
    allows detection of duplicate symbols.

---
+++ tests/test_compile_units.cpp
@@ -0,0 +1,19 @@
+/**
+This file is part of jtypes.
+
+Copyright(C) 2016 Christoph Heindl
+All rights reserved.
+
+This software may be modified and distributed under the terms
+of MIT license. See the LICENSE file for details.
+*/
+
+#include "catch.hpp"
+
+#include <jtypes/jtypes.hpp>
+
+
+TEST_CASE("jtypes")
+{
+}
+
